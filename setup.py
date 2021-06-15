2f982091a7a54bae503704c3374d3ee6bc920fb5
oscarkremer <oscar.s.kremer@hotmail.com>
2021-06-15 20:32:13 -0300

    refac: remove unecessary folder structure for error and mentor scripts

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='mentor',  
      version='0.1',
-     scripts=['mentor'] ,
+     scripts=['mentor/mentor.py', 'mentor/error.py'] ,
      author="Oscar Schmitt Kremer",
      author_email="oscar.s.kremer@hotmail.com",
-     description="A package to simulate direct and inverse kinematics of Mentor Didactic Robotic Arm",
+     description="A package to implement a physical model of the Mentor \\
+     didactic robotic arm. The package includes of direct and inverse kinematics.',
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/oscarkremer/mentor",
@@ -21,4 +22,9 @@ setuptools.setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
- )
+    install_requires=[
+        'itertools',
+        'numpy',
+        'pytest'
+    ]
+)
--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .input import input_angles, input_cartesian
-
-__all__ = [
-    'input',
-]
